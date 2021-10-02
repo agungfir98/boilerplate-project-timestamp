d615f5173c7c46a8cb309f644290364df9a73b4f
Agung Firmansyah <agungfir98@gmail.com>
2021-10-02 19:30:28 +0700

    TimeStamp project

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("/api/hello", function (req, res) {
 
 let PORT = 3000
 // listen for requests :)
-var listener = app.listen(PORT, function () {
+var listener = app.listen(process.env.PORT||PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
